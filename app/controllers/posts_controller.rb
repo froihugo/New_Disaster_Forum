5124d56af8884dbccae7c3a88f6be5263deb11e8
froihugo <jayfhugo@gmail.com>
2022-11-08 16:39:59 +0800

    fix destroy button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -35,6 +36,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :location, presence: true
+
+  belongs_to :user
+
 end
