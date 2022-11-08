dcfb3a49c63c5c45a76097af03d60ba7461babf7
froihugo <jayfhugo@gmail.com>
2022-11-08 15:17:23 +0800

    edit string to text due to maximum characters issue

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2022_11_08_050533) do
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
-    t.string "content"
+    t.text "content"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
