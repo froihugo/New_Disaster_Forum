b3f2920180af547448d300025b06d0462604db33
froihugo <jayfhugo@gmail.com>
2022-11-08 13:43:09 +0800

    add Posts Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @posts = Post.new
+  end
+
+  def create
+    post = Post.new(params[:post].permit(:title, :content, :location))
+    if post.save
+      redirect_to posts_path
+    else
+    end
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :location, presence: true
 end
