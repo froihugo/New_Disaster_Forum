3e5e30cc8d8da1b4cdc4da443d0997bb50f47ab0
froihugo <jayfhugo@gmail.com>
2022-11-08 14:16:02 +0800

    add delete and edit controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,47 @@
 class PostsController < ApplicationController
 
-  def index
-    @posts = Post.all
-  end
+  def index; @posts = Post.all end
 
-  def new
-    @posts = Post.new
-  end
+  def new; @posts = Post.new end
 
   def create
-    post = Post.new(params[:post].permit(:title, :content, :location))
-    if post.save
+    @post = Post.new(params[:post].permit(:title, :content, :location))
+    if @post.save
       redirect_to posts_path
     else
       render :new, status: :unprocessable_entity
     end
+  end
 
-    def show
-      @post = Post.find(params[:id])
+    def show; @post = Post.find(params[:id]) end
+
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params.require(:post).permit(:title, :content, location))
+      redirect_to posts_path
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, category_ids: [])
   end
 
 end
