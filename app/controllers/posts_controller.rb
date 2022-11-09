a65a22b9fa4f1eb86432951013bc77be0af359f2
froihugo <jayfhugo@gmail.com>
2022-11-09 18:12:00 +0800

    fix writing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  # before_action :validate_post_owner, only: [:edit, :update, :destroy]
+  #before_action :validate_post_owner, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
