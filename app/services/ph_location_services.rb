0fd02e1a5a34df317cc2ac55f1e617f5dc856961
froihugo <jayfhugo@gmail.com>
2022-11-11 18:11:51 +0800

    add fetch region and provinces

---
+++ app/services/ph_location_services.rb
@@ -0,0 +1,30 @@
+class PhLocationService
+  attr_reader :url
+
+  def initialize
+    @url = 'https://psgc.gitlab.io/api'
+  end
+
+  def fetch_region
+    request = RestClient.get("#{url}/regions/")
+    data = JSON.parse(request.body)
+    data.each do |region|
+      address_region = Address::Region.find_or_initialize_by(code: region['code'])
+      address_region.name = region['regionName']
+      address_region.save
+    end
+  end
+
+  def fetch_province
+    request = RestClient.get("#{url}/provinces")
+    data = JSON.parse(request.body)
+    data.each do |province|
+      address_province = Address::Province.find_or_initialize_by(code: province['code'])
+      region = Address::Region.find_by_code(province['regionCode'])
+      address_province.region = region
+      address_province.name = province['name']
+      address_province.save
+    end
+  end
+
+end
