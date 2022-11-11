3e96790691848abb1f0a098a522e9ebc39422ff3
froihugo <jayfhugo@gmail.com>
2022-11-11 17:23:34 +0800

    add authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_11_040521) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_11_084038) do
   create_table "comments", charset: "utf8mb4", force: :cascade do |t|
     t.text "content"
     t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
@@ -27,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_11_040521) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "unique_string"
+    t.string "ip_address"
+    t.integer "comments_count", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
