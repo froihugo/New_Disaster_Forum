fa5fd8c443a745e050a581f4d9900702c458ef0f
froihugo <jayfhugo@gmail.com>
2022-11-14 10:57:22 +0800

    done posts index action , json format render by serializer done show action api format

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,10 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @posts, each_serializer: PostSerializer }
+    end
   end
 
   def new
@@ -30,6 +34,10 @@ class PostsController < ApplicationController
 
     def show
       @post = Post.find(params[:id])
+      respond_to do |format|
+        format.html
+        format.json { render json: @post, serializer: PostSerializer }
+      end
     end
 
 
