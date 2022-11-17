073f0b00d1ef33d94b5c4ea87b6012d36bb2dadd
froihugo <jayfhugo@gmail.com>
2022-11-17 14:02:14 +0800

    add javascript for copy function

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,8 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+
+import jQuery from "jquery"
+window.jQuery = jQuery
+window.$ = jQuery
---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,11 @@
+import {Controller} from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = ["email"]
+
+    copy() {
+        navigator.clipboard.writeText(this.emailTarget.textContent);
+        document.getElementsByClassName("notice")[0].innerText = this.emailTarget.textContent + ' was copied successfully'
+    }
+
+    }
