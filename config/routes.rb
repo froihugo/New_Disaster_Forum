3378adbe86a3d274d747fd6c96134c162b131656
froihugo <jayfhugo@gmail.com>
2022-11-23 13:09:33 +0800

    add routes for admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
   root 'home#Index'
+
   resources :categories
   resources :posts do
     resources :comments, except: :show
@@ -9,9 +11,24 @@ Rails.application.routes.draw do
 
   get "/:unique_string", to: "posts#short_url"
 
-    namespace :api do
-      resources :regions, only: :index, defaults: { format: :json } do
-        resources :provinces, only: :index, defaults: { format: :json }
+  constraints(AdminDomainConstraint.new) do
+    namespace :admin do
+      resources :users
+    end
+  end
+
+  namespace :api do
+    resources :regions, only: :index, defaults: { format: :json } do
+      resources :provinces, only: :index, defaults: { format: :json } do
+        resources :city_municipalities, only: :index, defaults: { format: :json } do
+          resources :barangays, only: :index, defaults: { format: :json }
+        end
+      end
+      resources :districts, only: :index, defaults: { format: :json } do
+        resources :city_municipalities, only: :index, defaults: { format: :json } do
+          resources :barangays, only: :index, defaults: { format: :json }
+        end
       end
     end
   end
+end #end
